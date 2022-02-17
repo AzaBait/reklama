4b6610ef29d8ce156e8d3813de14b680c2bcf8c7
dell <m>
2022-02-17 18:41:33 +0600

    Initial commit

---
+++ src/main/java/azamat/demo/ReklamaApplication.java
@@ -0,0 +1,14 @@
+package azamat.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReklamaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReklamaApplication.class, args);
+    }
+    
+
+}
---
+++ src/test/java/azamat/demo/ReklamaApplicationTests.java
@@ -0,0 +1,13 @@
+package azamat.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReklamaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
